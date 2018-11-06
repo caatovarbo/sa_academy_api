f602eb987e51feb87a7ac482e9bd51467b45deca
bsdiaza@unal.edu.co <bsdiaza@unal.edu.co>
2018-11-05 20:05:20 -0500

    students api

---
+++ src/students/typeDefs.js
@@ -0,0 +1,24 @@
+export const studentsTypeDef = `
+type Student {
+    code: Int!
+    name: String!
+    credits: Int!
+    professor: String!
+}
+input StudentInput {
+    name: String!
+    credits: Int!
+    professor: String!
+}`;
+
+export const studentsQueries = `
+    allStudents: [Student]!
+    studentByCode(code: Int!): Student!
+`;
+
+export const studentsMutations = `
+    createStudent(student: StudentInput!): Student!
+    deleteStudent(code: Int!): Int
+    updateStudent(code: Int!, student: StudentInput!): Student!
+    loginStudent(student: StudentInput!): String!
+`;
